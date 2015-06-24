df90132bfc4ee3590c1e332e961f02a7cb660c58
MR.Y <programming@yatsukouji.com>
2015-06-24 09:37:41 +0900

    db/seeds.rbにCompany.createを追加 times内にcompany_id: rand(3) + 1を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,17 @@
 # 100.times do |index|
 #   Customer.create(family_name: "鈴木", given_name: "太郎", email: "customer_#{index}@sparta.com")
 # end
+Company.create(name: "スパルタ商事", url: "http://spartacamp.jp/", address: "東京都千代田区1-1-1")
+Company.create(name: "スパルタ物産", url: "http://spartacamp.jp/", address: "東京都千代田区1-1-2")
+Company.create(name: "スパルタ銀行", url: "http://spartacamp.jp/", address: "東京都千代田区1-1-3")
+
 
 100.times do |index|
   Customer.create(
     family_name: Faker::Japanese::Name.last_name,
     given_name: Faker::Japanese::Name.first_name,
-    email: "customer_#{index}@sparta.com"
+    email: "customer_#{index}@sparta.com",
+    company_id: rand(3) + 1
   )
 end
 
