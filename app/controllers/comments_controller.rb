1f7297a84f68538e3a33dbfb5487163484fbb40d
MR.Y <programming@yatsukouji.com>
2015-06-26 08:07:02 +0900

    コメント削除のバグを修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,8 +11,14 @@ class CommentsController < ApplicationController
 
 
   def destroy
-
+    @comment = Comment.find(params[:id])
+    # @commentがdestroyされる前に、commentが誰のものかを変数に保存する
+    customer_id = @comment.customer_id
+    @comment.destroy
+    # さっき保存したcustomer_idをここで使う
+    redirect_to customer_url(customer_id)
   end
+
 private
 
   def comment_params
