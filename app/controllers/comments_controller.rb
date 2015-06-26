172e0ef28ccf2a69b245b9e8414d76c97ab76c12
MR.Y <programming@yatsukouji.com>
2015-06-26 09:52:05 +0900

    バグはありますが、いろいろ実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,20 @@
 class CommentsController < ApplicationController
 
   def create
-    @comment = Comment.new(comment_params)
-    if @comment.save
-      redirect_to customer_url(@comment.customer_id)
-    else
-      redirect_to customer_url(@comment.customer_id)
-    end
+      # 1.customer ID 取得
+    customer_id = comment_params["customer_id"].to_i
+
+    # 2.user ID 取得
+    user_id = current_user.id
+
+    # 3.body 取得（コメント本文）
+    body = comment_params["body"]
+
+    # 4.各データを持ったCommentのインスタンス作成
+    @comment = Comment.new(user_id: user_id, customer_id: customer_id, body: body)
+    @comment.save
+    redirect_to customer_url(customer_id)
+
   end
 
 
